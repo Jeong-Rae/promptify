0e5d8474530d56b7ad3dd6cedd4a4422e9474959
JRJR519 <kkwjdfo@gmail.com>
2025-04-12 04:43:52 +0900

    feat: ESLint 및 Prettier 설정 업데이트

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,28 +1,94 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+export default [
+    {
+        ignores: ["dist/**", "sample/**"],
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    eslint.configs.recommended,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: "module",
+                project: "./tsconfig.json",
+            },
+            globals: {
+                URL: "readonly",
+                process: "readonly",
+                describe: "readonly",
+                it: "readonly",
+                expect: "readonly",
+                beforeEach: "readonly",
+                afterAll: "readonly",
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+            "prettier": prettier,
+            "import": importPlugin,
+        },
+        settings: {
+            "import/resolver": {
+                typescript: {
+                    alwaysTryTypes: true,
+                },
+            },
+        },
+        rules: {
+            // Prettier 연동
+            "prettier/prettier": "error",
+
+            // TypeScript 규칙
+            "@typescript-eslint/explicit-function-return-type": "warn",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                    varsIgnorePattern: "^_",
+                    args: "none",
+                },
+            ],
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-floating-promises": "error",
+            "@typescript-eslint/no-misused-promises": "error",
+            "@typescript-eslint/consistent-type-imports": "error",
+            "@typescript-eslint/no-shadow": "error",
+
+            // 코드 클린업
+            "no-param-reassign": "error",
+
+            // Import 관련 규칙
+            "sort-imports": "off",
+            "import/order": [
+                "error",
+                {
+                    "groups": [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index",
+                        "object",
+                        "type",
+                    ],
+                    "newlines-between": "always",
+                    "alphabetize": {
+                        order: "asc",
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+            "import/no-cycle": "error",
+            "import/no-useless-path-segments": "error",
+        },
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+];
---
+++ apps/web/prettier.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+    // 기본 포맷팅 규칙
+    semi: true,
+    trailingComma: "es5",
+    singleQuote: false,
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: "always",
+    quoteProps: "consistent",
+    proseWrap: "always",
+    endOfLine: "auto",
+
+    // import 정렬 규칙 (Trivago 플러그인용)
+    importOrder: ["^node:", "<THIRD_PARTY_MODULES>", "^@/(.*)$", "^[./]"],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
