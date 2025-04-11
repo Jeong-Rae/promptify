86e63642dda6384d5cee689f9e470cfae64d384f
JRJR519 <kkwjdfo@gmail.com>
2025-04-12 05:52:04 +0900

    feat: @promptify/shared 패키지 추가 및 tsconfig 설정 업데이트, ESLint 구성 수정

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
+import { Theme } from "@radix-ui/themes";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Theme } from "@radix-ui/themes";
+
 import "./index.css";
 import App from "./App";
 
